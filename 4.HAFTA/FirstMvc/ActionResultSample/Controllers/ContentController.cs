eefd565d71be63e51906ceb9744f830ee5e5752e
edakarakas1 <72891447+edakarakas1@users.noreply.github.com>
2022-01-18 13:18:21 +0300

    .

---
+++ 4.HAFTA/FirstMvc/ActionResultSample/Controllers/ContentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionResultSample.Controllers
+{
+    public class ContentController : Controller
+    {//Kullanıcı tanımlı bir değer return etmek için kullanılır
+        // GET: Content
+        public ContentResult Index()
+        {
+            // return Content("eda karakas");
+            return Content("eda karakas", "text/html; charset = utf - 8") ;
+        }
+        public ContentResult Index2()
+        {
+            // return Content("<b>eda karakas</b>");
+            return Content("<b>eda karakas</b>", "text/plain");
+        }
+    }
+}
---
+++ 4.HAFTA/FirstMvc/ActionResultSample/Controllers/HttpController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionResultSample.Controllers
+{
+    public class HttpController : Controller
+    {//Http durum kodlarını report etmek için kullanılır
+        // GET: Http
+        public ActionResult Index()
+        {
+            // return new HttpStatusCodeResult(HttpStatusCode.BadRequest);//502hatası gelecek,
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest,"Bad Gateway hatası");
+        }
+        public HttpUnauthorizedResult Index2()
+        {
+            // return new HttpUnauthorizedResult();
+            return new HttpUnauthorizedResult("Yetkili Değilsiniz");
+        }
+        public HttpNotFoundResult Index3()
+        {
+            //return HttpNotFound();//sayfa bulunamadı hatası
+            return HttpNotFound("Sorry!This page not found");
+        }
+    }
+}
