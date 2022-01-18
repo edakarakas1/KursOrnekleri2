10a34bdb106ba2781e95a5ba8a9f86417183389b
edakarakas1 <72891447+edakarakas1@users.noreply.github.com>
2022-01-18 13:11:39 +0300

    .

---
+++ 4.HAFTA/uygulama2/uygulama2/script.js
@@ -0,0 +1,20 @@
+﻿$(function ()
+{
+    checkbackgroundcolor();
+    $('.background a').on('click', function (e) {
+
+        e.preventDefault();//Tıklandığında görevi yapmasın
+        var color = $(this).data('color');
+        // alert(color);
+        localStorage.color = color;
+        setBackgroundColor(color);
+    });
+    function setBackgroundColor(color) {
+        $('body').css('background-color', color);
+    }
+    function checkbackgroundcolor() {
+        if (localStorage.color) {
+            setBackgroundColor(localStorage.color);
+        }
+    }
+});
