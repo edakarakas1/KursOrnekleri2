97c19a9e3d064481f3f6835ab0a9773112cdeb30
edakarakas1 <72891447+edakarakas1@users.noreply.github.com>
2022-01-18 13:27:11 +0300

    .

---
+++ 4.HAFTA/MvcBasicModel/MvcBasicModel/Controllers/DataController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBasicModel.Controllers
+{
+    public class DataController : Controller
+    {
+        // GET: Data
+        public ActionResult Webmaster()
+        {
+            Master master = new Master();
+            master.id = 1001;
+            master.Ad = "eda";
+            master.Soyad = "karakas";
+            //1.yol viewbag
+            ViewBag.id = master.id;
+            ViewBag.Ad = master.Ad;
+            ViewBag.Soyad = master.Soyad;
+            ViewBag.Master = master;
+
+
+            //return View();
+            //2.yol Model göndermek .Model demek datanın kendisi
+            return View(master);
+        }
+
+        public ActionResult Personel()
+        {
+            List<Personel> plist = new List<Personel>();
+            Personel p = new Personel();
+            p.id = 1;
+            p.Ad = "Sena";
+            p.Soyad = "Mutlu";
+            p.Maas = 5000;
+            plist.Add(p);
+
+            Personel p2 = new Personel();
+            p2.id = 2;
+            p2.Ad = "Eda";
+            p2.Soyad = "Şahin";
+            p2.Maas = 5000;
+            plist.Add(p2);
+
+            Personel p3 = new Personel();
+            p3.id = 3;
+            p3.Ad = "Gül";
+            p3.Soyad = "Yılmaz";
+            p3.Maas = 5000;
+            plist.Add(p3);
+            return View(plist);
+        }
+    }
+}
