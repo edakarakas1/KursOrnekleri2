ca9d1518400b4497e924abbabae7ab60f251b18a
edakarakas1 <72891447+edakarakas1@users.noreply.github.com>
2022-01-20 14:27:48 +0300

    .

---
+++ 5.HAFTA/FirstMvc/ActionResultSample/Controllers/RedirectController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionResultSample.Controllers
+{
+    public class RedirectController : Controller
+    {//Bir url aracılığıyla hem uygulama içinde hem de uygulama dışında yönlendirme işlemi yapmak için kullanılır
+        // GET: Redirect
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public RedirectResult Index2()
+        {
+            // return Redirect("/view");
+            return Redirect("http://microsoft.com");
+        }
+        //redirecttoAction metodu ile sadece uygulama içerisindeki sayfalara yönlendirme gerçekleştirilir
+        public RedirectToRouteResult Index3()
+        {
+            // return RedirectToAction("Index");
+            // return RedirectToAction("Index", "View");
+           // return RedirectToAction("Index", new { controller = "View", id = 5 });
+           return RedirectToRoute( "Default",new {controller="View",action="Index2"});
+        }
+    }
+}
---
+++ 5.HAFTA/FirstMvc/FirstMvc/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //public string Index()
+        //{
+        //    return "Merhaba Dünya";
+        //}
+
+        public ActionResult Deneme()
+        {
+            return View();
+        }
+        //public string Merhaba()
+        //{
+        //    return "Merhaba Dünya";
+        //}
+    }
+}
